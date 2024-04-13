ec1f898c7a2ad3e5ba3ad16f5641eae69077ac24
Omar <omaremid2002@outlook.com>
2024-04-13 17:06:43 +0300

    JavaScript/85.maximal-rectangle.js

---
+++ JavaScript/85.maximal-rectangle.js
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=85 lang=javascript
+ *
+ * [85] Maximal Rectangle
+ */
+
+// @lc code=start
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+var maximalRectangle = function(matrix) {
+    if (matrix.length === 0) return 0;
+    
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+    const heights = new Array(cols).fill(0);
+    let maxArea = 0;
+    
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            heights[j] = matrix[i][j] === '1' ? heights[j] + 1 : 0;
+        }
+        maxArea = Math.max(maxArea, largestRectangleArea(heights));
+    }
+    
+    return maxArea;
+};
+
+var largestRectangleArea = function(heights) {
+    const stack = [];
+    let maxArea = 0;
+    
+    for (let i = 0; i <= heights.length; i++) {
+        while (stack.length > 0 && (i === heights.length || heights[i] < heights[stack[stack.length - 1]])) {
+            const height = heights[stack.pop()];
+            const width = stack.length === 0 ? i : i - stack[stack.length - 1] - 1;
+            maxArea = Math.max(maxArea, height * width);
+        }
+        stack.push(i);
+    }
+    
+    return maxArea;
+};
+
+// @lc code=end
+
